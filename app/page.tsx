977dc844ec24dec89e47acb1a42e4942e87d8a96
coded-zeus <ibrahim.techstack@gmail.com>
2025-01-23 13:13:42 +0100

    style guide

--- app/page.tsx
+++ app/page.tsx
@@ -16,12 +16,8 @@ export default function Home() {
         <ol className="list-inside list-decimal text-center font-[family-name:var(--font-geist-mono)] text-sm sm:text-left">
           <li className="mb-2">
             Get started by editing{" "}
-            <code className="rounded bg-black/[.05] px-1 py-0.5 font-semibold dark:bg-white/[.06]">
-              app/page.tsx
-            </code>
-            .
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li className="">Save and see your changes instantly.</li>
         </ol>
 
         <div className="flex flex-col items-center gap-4 sm:flex-row">
