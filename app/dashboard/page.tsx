94193d8b07ebff7be1e84f3ec41b3fa00113d8a4
coded-zeus <ibrahim.techstack@gmail.com>
2025-01-24 18:00:44 +0100

    dashboard layout

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+import DashboardWrapper from '@/components/shared/dashboard-wrapper'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <DashboardWrapper>
+      <div>
+        <h1>Dashboard</h1>
+      </div>
+    </DashboardWrapper>
+  )
+}
+
+export default Page
---
+++ components/shared/dashboard-wrapper.tsx
@@ -0,0 +1,36 @@
+import { AppSidebar } from "@/components/app-sidebar";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbList,
+} from "@/components/ui/breadcrumb";
+import { Separator } from "@/components/ui/separator";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+
+const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 border-b-2  px-4 bg-white">
+          <SidebarTrigger className="-ml-1 md:hidden" />
+          <Separator orientation="vertical" className="mr-2 h-4" />
+          <Breadcrumb>
+            <BreadcrumbList>
+              <BreadcrumbItem className="hidden  text-[21.30px] font-normal leading-relaxed text-[#1c2833] md:block">
+                👋🏻 Welcome back
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
+        </header>
+        {children}
+      </SidebarInset>
+    </SidebarProvider>
+  );
+};
+
+export default DashboardWrapper
